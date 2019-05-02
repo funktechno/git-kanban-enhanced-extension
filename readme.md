a96614c8b9c222a5a71e3ec970efc406faa46af9
lastlink <theefunk@gmail.com>
2019-05-02 13:28:07 -0400

    work on options page

--- readme.md
+++ readme.md
@@ -7,6 +7,10 @@
 
 * https://developer.chrome.com/extensions/getstarted
 
+## content locations
+
+* https://developer.chrome.com/extensions/match_patterns
+
 ## todo icon
 
 * need 16, 48, 128
---
+++ src/html/options.js
@@ -0,0 +1,34 @@
+// Saves options to chrome.storage
+function save_options() {
+    console.log("saving options")
+    var type = document.getElementById('gitType').value;
+    var url = document.getElementById('gitHost').checked;
+    chrome.storage.sync.set({
+        favoriteColor: color,
+        likesColor: likesColor
+    }, function () {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function () {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+    // Use default value color = 'red' and likesColor = true.
+    console.log("restoring options")
+    // chrome.storage.sync.get({
+    //     favoriteColor: 'red',
+    //     likesColor: true
+    // }, function (items) {
+    //     document.getElementById('color').value = items.favoriteColor;
+    //     document.getElementById('like').checked = items.likesColor;
+    // });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
