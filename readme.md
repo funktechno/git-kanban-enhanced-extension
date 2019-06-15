e1f2b01d49a529c07f75bb3a8085aa35579d7a8a
Mr. Funk <funk@funks-mac-pro-4.local>
2019-06-15 00:41:48 -0400

    init working vuejs

--- readme.md
+++ readme.md
@@ -3,6 +3,7 @@
 * Kanban extensions for github, gitlab, and bitbucket
 * should support weights, multi projects, epics, blockers
 * 2 plugins, one supports custom urls, one has predefined
+* initialized using [YuraDev/vue-chrome-extension-template](https://github.com/YuraDev/vue-chrome-extension-template)
 
 ## published chrome apps
 
@@ -12,6 +13,22 @@
 ## getting started
 
 * https://developer.chrome.com/extensions/getstarted
+* startup
+```bash
+$ npm install # or yarn
+$ npm run dev # or yarn dev
+```
+
+## linter...
+
+* install `https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint`
+* add `"eslint.autoFixOnSave": true` to settings.json
+
+## node version support
+
+* `nvm install 8.16.0` use a node version less than 9
+    * `nvm use 8.16.0`
+* `npm rebuild node-sass` rebuild sass using 8
 
 ## content locations
 
---
+++ src/tab/root.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="custom-options" style="display:none;">
+      <h3>Custom Git Platform:</h3>
+      <ul id="git-hosts">
+        <li>git.coolaj86.com : gitea.io <button>Remove</button> </li>
+      </ul>
+    </div>
+    <p></p>
+    <div class="custom-options" style="display:none;">
+      <h3>Add a custom git platform:</h3>
+      <label>Url Host: </label>
+      <input id="gitHost" placeholder="github.example.com">
+      <br>
+      <label>Git Host Type: </label>
+      <select id="gitType">
+        <option value="github.com">github.com (Not Yet Supported)</option>
+        <option value="bitbucket.org">bitbucket.org (Not Yet Supported)</option>
+        <option value="gitlab.com">gitlab.com (Not Yet Supported)</option>
+        <option value="gitea.io">gitlea.com (Not Yet Supported)</option>
+      </select>
+      <button id="save">Add</button>
+
+    </div> 
+    <!-- end option add -->
+
+    <div id="status"></div>
+
+    <div>
+      <h3>Kanban Labels: {{test}}</h3>
+      <ul>
+        <li>backlog</li>
+        <li>analysis</li>
+        <li>in-progress</li>
+        <li>testing</li>
+        <li>closed</li>
+      </ul>
+    </div>
+  </div>
+    <!-- div Git Kanban Options
+    div {{test}} -->
+</template>
+<script>
+  export default {
+    data: () => ({
+      test: 'candy1'
+    }),
+    computed: { },
+    created () { },
+    mounted () {
+      console.log("saving options")
+      var type = document.getElementById('gitType').value
+      var url = document.getElementById('gitHost').value
+      console.log(type + url)
+    },
+    methods: { }
+  }
+</script>
+<style lang="scss">
+  div {
+    color: blue
+  }
+</style>
