f1716c55c5ac6a96e19633130900cda7a08bc5e1
lastlink <theefunk@gmail.com>
2020-11-02 23:39:32 -0500

    tab test

--- tests/unit/tab/tab.spec.ts
+++ tests/unit/tab/tab.spec.ts
@@ -6,7 +6,10 @@ describe("tab/tab", () => {
     const msg = "Add a custom git platform: Url Host:    Git Host Type:  github.com (Not Yet Supported) bitbucket.org (Not Yet Supported) gitlab.com (Not Yet Supported) gitea.com (kanban works) Add  Kanban Labels: backlog analysis in-progress testing closed Export/Import    Import Export";
     const responseDefault = Promise.resolve({
       status: 200,
-      data: {}
+      data: {},
+      body: {
+        name: "Self Hosted Kanban"
+      }
     });
     const wrapper = shallowMount(App, {
       mocks: {
@@ -18,7 +21,10 @@ describe("tab/tab", () => {
         }
       },
       // propsData: { msg },
-    });
+    }); 
+    // await wrapper.vm.$nextTick();
+    // await responseDefault.then();
+    // await wrapper.vm.$nextTick();
     expect(wrapper.text()).toMatch(msg);
   });
 });
